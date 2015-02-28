c8a94bd4e33ff3478be889610ae09f519d06c25a
acraven <andrew@epworth-consulting.co.uk>
2015-02-28 00:22:32 +0000

    Add script to update connection strings for appveyor

---
+++ UpdateTestConfigsForAppVeyor.ps1
@@ -0,0 +1,7 @@
+# Replace connection strings for AppVeyor SQLServer 2012 Express
+$startPath = "$($env:appveyor_build_folder)\src\Atlas.Persistence.EntityFramework.Tests"
+$config = join-path $startPath "app.config"
+$doc = (gc $config) -as [xml]
+$doc.SelectSingleNode('//connectionStrings/add[@name="Persistence"]').connectionString = "Server=(local)\SQL2012SP1;Database=Persistence;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
+$doc.SelectSingleNode('//connectionStrings/add[@name="PersistenceEF"]').connectionString = "Server=(local)\SQL2012SP1;Database=PersistenceEF;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
+doc.Save($config)
