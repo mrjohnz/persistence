5e427e189e7c85164935479cfb9e4d6ec84f5c2e
acraven <andrew@epworth-consulting.co.uk>
2015-02-28 00:43:37 +0000

    Update database name in script

--- UpdateTestConfigsForAppVeyor.ps1
+++ UpdateTestConfigsForAppVeyor.ps1
@@ -2,6 +2,6 @@
 $startPath = "$($env:appveyor_build_folder)\src\Atlas.Persistence.EntityFramework.Tests"
 $config = join-path $startPath "app.config"
 $doc = (gc $config) -as [xml]
-$doc.SelectSingleNode('//connectionStrings/add[@name="Persistence"]').connectionString = "Server=(local)\SQL2012SP1;Database=Persistence;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
-$doc.SelectSingleNode('//connectionStrings/add[@name="PersistenceEF"]').connectionString = "Server=(local)\SQL2012SP1;Database=PersistenceEF;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
+$doc.SelectSingleNode('//connectionStrings/add[@name="Persistence"]').connectionString = "Server=(local)\SQL2012SP1;Database=AtlasPersistenceTests;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
+$doc.SelectSingleNode('//connectionStrings/add[@name="PersistenceEF"]').connectionString = "Server=(local)\SQL2012SP1;Database=AtlasPersistenceTests_EntityFramework;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
 $doc.Save($config)
