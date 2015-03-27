2d9b20b295b460dc8d0509e3f6f433acd8677f80
acraven <andrew@epworth-consulting.co.uk>
2015-03-27 22:23:28 +0000

    Update NHibernate tests config during build

--- UpdateTestConfigsForAppVeyor.ps1
+++ UpdateTestConfigsForAppVeyor.ps1
@@ -5,3 +5,11 @@ $doc = (gc $config) -as [xml]
 $doc.SelectSingleNode('//connectionStrings/add[@name="Persistence"]').connectionString = "Server=(local)\SQL2012SP1;Database=AtlasPersistenceTests;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
 $doc.SelectSingleNode('//connectionStrings/add[@name="PersistenceEF"]').connectionString = "Server=(local)\SQL2012SP1;Database=AtlasPersistenceTests_EntityFramework;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
 $doc.Save($config)
+
+$startPath = "$($env:appveyor_build_folder)\src\Atlas.Persistence.NHibernate.Tests"
+$config = join-path $startPath "app.config"
+$doc = (gc $config) -as [xml]
+$doc.SelectSingleNode('//connectionStrings/add[@name="Persistence"]').connectionString = "Server=(local)\SQL2012SP1;Database=AtlasPersistenceTests;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
+$doc.SelectSingleNode('//connectionStrings/add[@name="PersistenceNHByCode"]').connectionString = "Server=(local)\SQL2012SP1;Database=AtlasPersistenceTests_NHibernateByCode;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
+$doc.SelectSingleNode('//connectionStrings/add[@name="PersistenceNHFluent"]').connectionString = "Server=(local)\SQL2012SP1;Database=AtlasPersistenceTests_FluentNHibernate;User ID=sa;Password=Password12!;MultipleActiveResultSets=True"
+$doc.Save($config)
